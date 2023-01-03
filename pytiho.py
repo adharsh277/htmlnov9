6e2fde690c52e9766993aa6a658ff6e42daf772e
adharsh.U <114822057+adharsh277@users.noreply.github.com>
2023-01-04 00:48:16 +0530

    Create pytiho.py

---
+++ pytiho.py
@@ -0,0 +1,21 @@
+x = "optimised of the colabaration of the domesticated to the celebratioan "
+y = "what peoples think about me is likea ashit to me i didn't want o be think like me "
+print(x)
+print(y)
+print(x.split())
+print(y.split())
+print(x.upper())
+print(y.upper())
+# 37r82r
+a = 98
+b = 34
+if a > b:
+    print("leo messi is the greatest of all time")
+else:
+    print("cristano is the greatest")
+x = 50000
+y = 50000
+if a == b:
+    print("huijdgshye")
+else:
+    print("skhiasudn")
